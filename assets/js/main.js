d2975d04c0015a4345e9f8a5488d2e7b706e705b
sofiacmachado <85806152+sofiacmachado@users.noreply.github.com>
2022-11-17 22:03:41 +0000

    post

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,7 +112,7 @@
       </div>
 
       <div class='col-lg-6'>
-          <form action="https://formsubmit.co/sofia_cmachado@hotmail.com" method='post' role='form'>
+          <form action="https://formsubmit.co/sofia_cmachado@hotmail.com" method='POST' role='form'>
             <div class='form-row'>
               <div class='col-md-6 form-group'>
                 <input type='text' name='name' class='form-control' id='name' placeholder='Your Name' data-rule='minlen:4' data-msg='Please enter at least 4 chars' required>
